03e9d2973ab120e515bd3f6afe0269d0d199af6a
hermes7144 <kichancho@gmail.com>
2022-05-25 22:36:11 +0900

    로그인 폼

---
+++ blog-frontend/src/modules/auth.js
@@ -0,0 +1,46 @@
+// 리듀서
+import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
+// 액션타입 생성
+const CHANGE_FIELD = 'auth/CAHNGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+// 액션 생성 함수 정의
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form, // register, login
+    key, // username, password, passwordConfirm
+    value, // 실제 바꾸려는 값
+  }),
+)
+export const initializeForm = createAction(INITIALIZE_FORM, form => form); // register / login
+
+// 초깃값 설정
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  }
+}
+// 리듀서 함수
+const auth = handleActions(
+  {
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, draft => {
+        draft[form][key] = value; // 예: state.register.username을 바꾼다.
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    })
+  },
+  initialState,
+)
+
+export default auth;
