04dcf340c2891e67f83e07eaa62bfba6ce224942
hermes7144 <kichancho@gmail.com>
2022-05-31 23:52:25 +0900

    리덕스 리듀서 구현중

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,17 +1,20 @@
 // 리듀서
 import { createAction, handleActions } from 'redux-actions';
 import produce from 'immer';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestSaga'
+import * as authAPI from '../lib/api/auth'
 // 액션타입 생성
 const CHANGE_FIELD = 'auth/CAHNGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-const REGISTER = 'auth/REGISTER';
-const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
-const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+const [REGISTER, REGISTER_SUCCESS, REGISTER_FAILURE] = createRequestActionTypes(
+  'auth/REGISTER',
+);
 
-const LOGIN = 'auth/LOGIN';
-const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
-const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
+const [LOGIN, LOGIN_SUCCESS, LOGIN_FAILURE] = createRequestActionTypes(
+  'auth/LOGIN',
+)
 
 // 액션 생성 함수 정의
 export const changeField = createAction(
@@ -23,6 +26,7 @@ export const changeField = createAction(
   }),
 )
 export const initializeForm = createAction(INITIALIZE_FORM, form => form); // register / login
+
 
 // 초깃값 설정
 const initialState = {
