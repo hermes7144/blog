5e8b8f85882ae616c704c06415ae72b24c3b9623
hermes7144 <kichancho@gmail.com>
2022-06-08 22:58:36 +0900

    write 완료

--- blog-frontend/src/containers/auth/LoginForm.js
+++ blog-frontend/src/containers/auth/LoginForm.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
-
 import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
@@ -35,6 +34,11 @@ const LoginForm = () => {
     dispatch(login({ username, password }))
   }
 
+  // 컴포넌트가 처음 렌더링 될 때 form 을 초기화함
+  useEffect(() => {
+    dispatch(initializeForm('login'));
+  }, [dispatch]);
+
   useEffect(() => {
     if (authError)
     {
@@ -48,8 +52,6 @@ const LoginForm = () => {
       console.log('로그인 성공')
       dispatch(check());
     }
-
-
   }, [auth, authError, dispatch])
 
   useEffect(() => {
@@ -66,8 +68,6 @@ const LoginForm = () => {
     }
   }, [navigate, user])
 
-
-
   return (
     <AuthForm
       type='login'
@@ -78,8 +78,5 @@ const LoginForm = () => {
     ></AuthForm>
   )
 }
-// 위 컴포넌트에서는 onChange 함수와 onSubmit 함수를 구현하여 필요한 액션을 디스패치하도록 구현해 주었습니다.
-// 또한, useEffect를 사용하여 맨 처음 렌더링 후 initializeForm 액션 생성 함수를 호출했습니다.
-
 
 export default LoginForm
---
+++ blog-frontend/src/containers/write/WriteActionButonContainer.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import WriteActionButtons from '../../components/write/WriteActionButtons'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { writePost } from '../../modules/write';
+
+const WriteActionButonContainer = ({ history }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { title, body, tags, post, postError } = useSelector(({ write }) => ({
+    title: write.title,
+    body: write.body,
+    tags: write.tags,
+    post: write.post,
+    postError: write.postError,
+  }));
+
+  // 포스트 등록
+  const onPublish = () => {
+    dispatch(
+      writePost({
+        title,
+        body,
+        tags,
+      }),
+    );
+  };
+
+  // 취소
+  const onCancel = () => {
+    history.goBack();
+  };
+
+  useEffect(() => {
+    if (post)
+    {
+      const { _id, user } = post;
+      navigate(`/@${user.username}/${_id}`);
+    }
+    if (postError)
+    {
+      console.log(postError);
+    }
+  }, [navigate, history, post, postError])
+
+  return (
+    <WriteActionButtons onPublish={onPublish} onCancel={onCancel} />
+  )
+}
+
+export default WriteActionButonContainer
